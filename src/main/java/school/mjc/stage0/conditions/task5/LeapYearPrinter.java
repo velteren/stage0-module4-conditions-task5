0c7f775ebfc290f5441c344ef2b7445c118045eb
Constantine <77935386+velteren@users.noreply.github.com>
2023-12-18 11:40:41 +0300

    fix: errors

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.conditions.task5;
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
         String result = year % 100 == 0 && year % 400 == 0 ? "leap" : "not leap";
-        result = result == "not leap" && year % 4 == 0 ? "leap" : "not leap";
+        result = result == "not leap" && year % 4 == 0 && year % 100 !=0 ? "leap" : "not leap";
         System.out.println(result);
     }
 }
