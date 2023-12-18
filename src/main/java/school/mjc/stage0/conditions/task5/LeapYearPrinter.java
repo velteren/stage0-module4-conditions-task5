dc4d00eb64b7e3ba5094d4d48a195146161b99aa
Constantine <77935386+velteren@users.noreply.github.com>
2023-12-18 10:56:01 +0300

    feat: add solution

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        String result = year % 4 == 0 ? "leap" : "not leap";
+        System.out.println(result);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -2,5 +2,35 @@ package school.mjc.stage0.conditions.task5;
 
 public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
+        int days;
+
+        switch (month) {
+            case (1):
+            case (3):
+            case (5):
+            case (7):
+            case (8):
+            case (10):
+            case (12):
+                days = 31;
+                break;
+            case (2):
+                days = 28;
+                break;
+            case (4):
+            case (6):
+            case (9):
+            case (11):
+                days = 30;
+                break;
+            default:
+                days = 0;
+        }
+
+        if (days > 0) {
+            System.out.println(days);
+        } else {
+            System.out.println("Error");
+        }
     }
 }
